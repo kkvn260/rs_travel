529d8dcd28669fbc2fc3324c0576c601c14b5419
Dolp <kkvn260@gmail.com>
2023-05-30 22:36:14 +0900

    메인화면 작업중

---
+++ lib/config/Palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/painting.dart';
+
+class Palette {
+  static Color textColor1 = const Color.fromARGB(66, 212, 202, 202);
+  static Color activeColor = const Color.fromARGB(66, 0, 0, 0);
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -14,22 +14,74 @@ class _MainTravelState extends State<MainTravel> {
       body: Stack(
         children: [
           Positioned(
+            top: 10,
+            right: 10,
+            left: 10,
+            bottom: 10,
             child: Container(
-              height: 200,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
                 image: const DecorationImage(
                   image: AssetImage('assets/images/main_.jpg'),
+                  fit: BoxFit.fill,
                 ),
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 15,
+                    spreadRadius: 5,
+                  ),
+                ],
               ),
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: '일정 생성'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: '일정 참여'),
+          Positioned(
+            top: MediaQuery.of(context).size.height / 2.5,
+            child: Container(
+              height: (MediaQuery.of(context).size.height / 2) - 10,
+              width: MediaQuery.of(context).size.width - 40,
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              padding: const EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.white.withOpacity(0.8),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: const [
+                          Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          Text(
+                            "Signin",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
