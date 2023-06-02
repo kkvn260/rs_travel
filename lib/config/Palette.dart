b3668a1c947bec554a302f245431c0da8c34be44
Dolp <kkvn260@gmail.com>
2023-06-02 15:46:52 +0900

    메인 UI 완료

--- lib/config/Palette.dart
+++ lib/config/Palette.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/painting.dart';
 
 class Palette {
-  static Color textColor1 = const Color.fromARGB(66, 212, 202, 202);
-  static Color activeColor = const Color.fromARGB(66, 0, 0, 0);
+  static Color textColor1 = const Color.fromARGB(66, 63, 59, 59);
+  static Color activeColor = const Color.fromARGB(255, 0, 0, 0);
+  static Color iconColor = const Color.fromARGB(255, 197, 181, 181);
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rs_travel/config/palette.dart';
 
 class MainTravel extends StatefulWidget {
   const MainTravel({super.key});
@@ -8,81 +9,311 @@ class MainTravel extends StatefulWidget {
 }
 
 class _MainTravelState extends State<MainTravel> {
+  // final _auth = FirebaseAuth.instance;
+  var loginTab = true;
+  var keyBoardOn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 10,
-            right: 10,
-            left: 10,
-            bottom: 10,
-            child: Container(
-              decoration: BoxDecoration(
-                image: const DecorationImage(
-                  image: AssetImage('assets/images/main_.jpg'),
-                  fit: BoxFit.fill,
-                ),
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
-                    blurRadius: 15,
-                    spreadRadius: 5,
+      resizeToAvoidBottomInset: false,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+          setState(() {
+            keyBoardOn = false;
+          });
+        },
+        child: Stack(
+          children: [
+            //배경
+            Positioned(
+              top: 10,
+              right: 10,
+              left: 10,
+              bottom: 10,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                    image: AssetImage('assets/images/main_.jpg'),
+                    fit: BoxFit.fill,
                   ),
-                ],
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 15,
+                      spreadRadius: 5,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height / 2.5,
-            child: Container(
-              height: (MediaQuery.of(context).size.height / 2) - 10,
-              width: MediaQuery.of(context).size.width - 40,
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.all(12.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.white.withOpacity(0.8),
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //텍스트 폼
+            Positioned(
+              top: keyBoardOn
+                  ? MediaQuery.of(context).size.height / 10
+                  : MediaQuery.of(context).size.height / 2.5,
+              right: 0,
+              left: 0,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+                height: loginTab
+                    ? (MediaQuery.of(context).size.height / 2) - 80
+                    : (MediaQuery.of(context).size.height / 2) - 10,
+                width: MediaQuery.of(context).size.width - 40,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: Colors.white.withOpacity(0.8),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
                     children: [
-                      Column(
-                        children: const [
-                          Text(
-                            "Login",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                loginTab = true;
+                                keyBoardOn = false;
+                              });
+                            },
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: loginTab
+                                        ? Palette.activeColor
+                                        : Palette.textColor1,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                if (loginTab)
+                                  Container(
+                                    margin: const EdgeInsets.only(top: 3),
+                                    height: 3,
+                                    width: 55,
+                                    color: Colors.blueGrey[500],
+                                  ),
+                              ],
                             ),
                           ),
-                        ],
-                      ),
-                      Column(
-                        children: const [
-                          Text(
-                            "Signin",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                loginTab = false;
+                                keyBoardOn = false;
+                              });
+                            },
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Signin",
+                                  style: TextStyle(
+                                    color: !loginTab
+                                        ? Palette.activeColor
+                                        : Palette.textColor1,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                if (!loginTab)
+                                  Container(
+                                    margin: const EdgeInsets.only(top: 3),
+                                    height: 3,
+                                    width: 55,
+                                    color: Colors.blueGrey[500],
+                                  ),
+                              ],
                             ),
                           )
                         ],
-                      )
+                      ),
+                      if (loginTab)
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 30),
+                          child: Form(
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  onTap: () {
+                                    setState(() {
+                                      keyBoardOn = true;
+                                    });
+                                  },
+                                  key: const ValueKey(1),
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(
+                                        Icons.email_rounded,
+                                        color: Palette.iconColor,
+                                      ),
+                                      hintText: "이메일",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      )),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextFormField(
+                                  obscureText: true,
+                                  onTap: () {
+                                    setState(() {
+                                      keyBoardOn = true;
+                                    });
+                                  },
+                                  key: const ValueKey(2),
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(
+                                        Icons.password,
+                                        color: Palette.iconColor,
+                                      ),
+                                      hintText: "비밀번호",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      )),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      if (!loginTab)
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 30),
+                          child: Form(
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  onTap: () {
+                                    setState(() {
+                                      keyBoardOn = true;
+                                    });
+                                  },
+                                  key: const ValueKey(3),
+                                  keyboardType: TextInputType.name,
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(
+                                        Icons.account_circle,
+                                        color: Palette.iconColor,
+                                      ),
+                                      hintText: "닉네임",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      )),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextFormField(
+                                  onTap: () {
+                                    setState(() {
+                                      keyBoardOn = true;
+                                    });
+                                  },
+                                  key: const ValueKey(4),
+                                  keyboardType: TextInputType.emailAddress,
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(
+                                        Icons.email_rounded,
+                                        color: Palette.iconColor,
+                                      ),
+                                      hintText: "이메일",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      )),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextFormField(
+                                  obscureText: true,
+                                  onTap: () {
+                                    setState(() {
+                                      keyBoardOn = true;
+                                    });
+                                  },
+                                  key: const ValueKey(5),
+                                  decoration: InputDecoration(
+                                      prefixIcon: Icon(
+                                        Icons.password,
+                                        color: Palette.iconColor,
+                                      ),
+                                      hintText: "비밀번호",
+                                      enabledBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderRadius: BorderRadius.circular(25),
+                                      )),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+            //버튼
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeIn,
+              top: keyBoardOn
+                  ? loginTab
+                      ? MediaQuery.of(context).size.height / 8
+                      : MediaQuery.of(context).size.height / 4.5
+                  : null,
+              bottom: loginTab
+                  ? MediaQuery.of(context).size.height / 6
+                  : MediaQuery.of(context).size.height / 15,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Colors.grey,
+                        Colors.white,
+                      ],
+                    ),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_forward),
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
